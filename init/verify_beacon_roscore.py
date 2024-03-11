34ed488d6ffe902d55a45198967d6aef040a509c
Waldo Le Robot <sudribot@outlook.com>
2024-03-11 23:22:02 +0000

    Correction script de lancement

--- init/verify_beacon_roscore.py
+++ init/verify_beacon_roscore.py
@@ -6,11 +6,11 @@ import subprocess
 
 FILE_PATH = os.path.abspath(__file__)
 FILE_NAME = os.path.basename(FILE_PATH)
-sys.path.insert(1, FILE_PATH.split("_1_ROS")[0]) #add parent folder to python path
+sys.path.insert(1, FILE_PATH.split("init")[0]) #add parent folder to python path
 from init import network_manager
 
 #Load config file   
-CONFIG_FILEPATH = FILE_PATH.split("_1_ROS")[0]+"init/configuration.json"
+CONFIG_FILEPATH = FILE_PATH.split("init")[0]+"init/configuration.json"
 with open (CONFIG_FILEPATH, "r") as f:
     config = json.load(f)
 
@@ -22,21 +22,21 @@ def start_verification():
     """
 
     #1st : Robot Waldo wifi must be connected to TP-Link network
-    if not network_manager.get_ipv4("wlan0") == config["SELF_IP_ADDRESS_ON_TP_LINK"]:
+    if not network_manager.get_ipv4("wlan0") == config["SELF_IP_ADDRESS_ON_TPLINK"][0]:
         #Condition not satisfied
         print(f"Log [{FILE_NAME}]: Impossible de se connecter au Louise roscore. Vous devez vous connecter"+\
-            f" au TP Link afin d'avoir l'addresse IP suivante : {config["SELF_IP_ADDRESS_ON_TP_LINK"]}")
+            f" au TP Link afin d'avoir l'addresse IP suivante : {config['SELF_IP_ADDRESS_ON_TPLINK'][0]}")
         return False
 
     #2nd : Beacon roscore must already run on the TP-Link network
     #Run netcat command to ping beacon specific ROS port
-    nc_string_bytes = subprocess.run(["nc","-zv", config["LOUISE_IP_ON_TPLINK"], "11311", "-w", "0.6"],
+    nc_string_bytes = subprocess.run(["nc","-zv", config["LOUISE_IP_ON_TPLINK"][0], "11311", "-w", "0.6"],
                                     stdout=subprocess.PIPE)
     nc_string = nc_string_bytes.stdout.decode('ascii')
     if not nc_string.split()[-1] == "succeeded!":
         #Condition not satisfied
         print(f"Log [{FILE_NAME}]: Louise roscore non détecté. Vérifiez que Louise à bien lancer"+\
-            f" son roscore sur son addresse IP TP Link ({config["LOUISE_IP_ON_TPLINK"]}).")
+            f" son roscore sur son addresse IP TP Link ({config['LOUISE_IP_ON_TPLINK'][0]}).")
         return False
 
 
